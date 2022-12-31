2ccd70da99e4fc3a38455fa65df78afee5cf3253
Aman Abdirazak <amanabdirizak@icloud.com>
2022-12-30 19:58:11 -0600

    function created to put input values into an object

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,24 @@ $(document).ready(onReady);
 
 function onReady() {
   console.log("hi. let's make a CRUD app.");
+  getStudents()
+  $('.submitButton').on('click', addStudent);
+}
+
+
+//function to take input values and put them in an object
+function addStudent() {
+  console.log('adding a student');
+  let newName = $('input').val();
+
+  let studentToSend = {
+    name: newName,
+  };
+
+  // postBooks(studentToSend);
+}
+
+//function that will request students + append to table
+function getStudents() {
+
 }
